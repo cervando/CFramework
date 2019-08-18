130c7f7f462552636514e11265b42424b120ea34
Armando <armando.cervantes.h@gmail.com>
2019-08-18 18:27:32 -0500

    Fixed TCP error

--- communications/p2p/sockets/TCPSocketListener.java
+++ communications/p2p/sockets/TCPSocketListener.java
@@ -7,54 +7,56 @@ import java.util.ArrayList;
 
 import cFramework.communications.BinaryArrayNotificable;
 import cFramework.communications.fiels.Address;
+import java.time.Clock;
 
 public class TCPSocketListener extends Thread{
 
-	Socket socket;
-	BinaryArrayNotificable listener;
-	
-	public TCPSocketListener( Socket sock, BinaryArrayNotificable listener){
-		this.socket = sock;
-		this.listener = listener;
-	}
-	
-	public void run(){
-		try {
-			DataInputStream in =
-			        new DataInputStream(socket.getInputStream());
-			
-			int incomingPort = in.readUnsignedShort();
-			int fragmentSize = 32000;
-			byte[] messageFragment = new byte[fragmentSize];
-			int readed = 0;
-			int messageLengt = 0;
-			ArrayList<byte[]> messContent = new ArrayList<byte[]>();
-			
-			while((readed = in.read(messageFragment)) > 0){
-				messageLengt += readed;
-				messContent.add(messageFragment.clone());
-			}
-			
-			int start = 0;
-			byte[] message = new byte[messageLengt];
-			for( byte[] fragment: messContent){
-				if ( start + fragmentSize > messageLengt )
-					System.arraycopy(fragment, 0, message, start, messageLengt % fragmentSize);
-				else	
-					System.arraycopy(fragment, 0, message, start, fragmentSize);
-				start += fragmentSize;
-			}
-			
-			Address addr = new Address(
-					socket.getInetAddress().toString().substring(1), 
-					incomingPort);
-			in.close();
-			socket.close();
-			listener.receive(addr, message);
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    Socket socket;
+    BinaryArrayNotificable listener;
+
+    public TCPSocketListener( Socket sock, BinaryArrayNotificable listener){
+        this.socket = sock;
+        this.listener = listener;
+    }
+
+    public void run(){
+        try {
+        
+            DataInputStream in =
+                new DataInputStream(socket.getInputStream());
+
+            int incomingPort = in.readUnsignedShort();
+            int fragmentSize = 32000;
+            byte[] messageFragment = new byte[fragmentSize];
+            int readed = 0;
+            int messageLengt = 0;
+            ArrayList<byte[]> messContent = new ArrayList<byte[]>();
+
+            while((readed = in.read(messageFragment)) > 0){
+                byte [] temp = new byte[readed];
+                System.arraycopy(messageFragment, 0, temp, 0, readed);
+                messageLengt += readed;
+                messContent.add(temp);
+            }
+
+            
+            int start = 0;
+            byte[] message = new byte[messageLengt];
+            for( byte[] fragment: messContent){
+                System.arraycopy(fragment, 0, message, start, fragment.length);
+                start += fragment.length;
+           }
+
+            Address addr = new Address(
+                            socket.getInetAddress().toString().substring(1), 
+                            incomingPort);
+            in.close();
+            socket.close();
+            listener.receive(addr, message);
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
